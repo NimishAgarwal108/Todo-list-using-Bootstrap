605dbbc4daea75b541f55137fe8469eadc81079e
Nimish Agarwal <agarwalnimish428@gmail.com>
2025-09-03 03:38:49 +0530

    updated localstorage

--- Todo app/src/App.jsx
+++ Todo app/src/App.jsx
@@ -5,52 +5,45 @@ import { useState } from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
 import styles from "./components/App.module.css";
 
-
 function App() {
-  const initialTodoItems=[
-    {
-      name:"Meeting",
-      dueDate:"06/09/2025",
-      done:false,
-    },
-    {
-      name:"Movie",
-      dueDate:"08/09/2025",
-      done:false,
-    },
-  ]
+  const [todoItems, setTodoItem] = useState(() => {
+    const savedTodos = localStorage.getItem("todoItems");
+    return savedTodos ? JSON.parse(savedTodos) : [
+      { name: "Meeting", dueDate: "06/09/2025", done: false },
+      { name: "Movie", dueDate: "08/09/2025", done: false }
+    ];
+  });
 
-  const [todoItems,setTodoItem]=useState(initialTodoItems);
-  const handleTodoItems=(itemName,itemDueDate)=>{
-    const newTodoItem=[...todoItems,{name:itemName,dueDate:itemDueDate,done:false}];
-    setTodoItem(newTodoItem);
+  const updateTodos = (newTodos) => {
+    setTodoItem(newTodos);
+    localStorage.setItem("todoItems", JSON.stringify(newTodos));
   };
 
-  const handleDelete=(todoItemName)=>{
-    const newTodoItem=todoItems.filter((item)=>item.name!==todoItemName);
-    setTodoItem(newTodoItem);
+  const handleTodoItems = (itemName, itemDueDate) => {
+    const newTodoItem = [...todoItems, { name: itemName, dueDate: itemDueDate, done: false }];
+    updateTodos(newTodoItem);
+  };
 
-
-  }
+  const handleDelete = (todoItemName) => {
+    const newTodoItem = todoItems.filter((item) => item.name !== todoItemName);
+    updateTodos(newTodoItem);
+  };
 
   const handleMarkDone = (todoName) => {
-  const updatedTodos = todoItems.map((item) =>
-    item.name === todoName ? { ...item, done:!item.done } : item
+    const updatedTodos = todoItems.map((item) =>
+      item.name === todoName ? { ...item, done: !item.done } : item
+    );
+    updateTodos(updatedTodos);
+  };
+
+  return (
+    <div className={styles['todo-container']}>
+      <AppName />
+      <AddTodo onNewItem={handleTodoItems} />
+      {todoItems.length === 0 && <WelcomeMessage />}
+      <TodoItems onDone={handleMarkDone} onDelete={handleDelete} todoItems={todoItems} />
+    </div>
   );
-  setTodoItem(updatedTodos);
-};
-
- 
-
-  return   (  
-     <div className={styles['todo-container']}>
-      <AppName/>
-      <AddTodo onNewItem={handleTodoItems}/>
-      {todoItems.length===0 && <WelcomeMessage></WelcomeMessage>}
-      <TodoItems onDone={handleMarkDone} onDelete={handleDelete} todoItems={todoItems} ></TodoItems>
-      </div>
- );
-  
 }
 
 export default App;
